2195dffc5b202551397299965fc7cd6462fa3323
niazleushkin <niazlv03@gmail.com>
2024-05-15 04:12:23 +0300

    feat: add Russian language translation (src/lang/locale/ru.ts)
    Added Russian language translation file for the application. Translated UI strings based on the original English file.

--- src/lang/locale/ru.ts
+++ src/lang/locale/ru.ts
@@ -1,3 +1,54 @@
 // русский
 
-export default {};
+// translated by @niazlv
+export default {
+    // Main.ts
+     CREATE_EXCEL: "Создать Excel Файл",
+     OPEN_AS_EXCEL: "Открыть в Excel",
+  
+     // ExcelView.ts
+     GET_FILE_FAILED: "Ошибка при получении файла",
+     READ_FILE_FAILED: "Ошибка чтения файла",
+     DATA_PARSING_ERROR: "Ошибка парсинга данных",
+     // TODO: Если кто знает, как лучше написать, исправьте. Речь об слове "встраимая".
+     // Я понятия не имею, как лучше всего перевести "embed" в данном контексте
+     COPY_EMBED_LINK: "Скопировать встроенную ссылку", 
+     COPY_EMBED_LINK_SUCCESS: "Скопировать встроенную ссылку в буфер обмена",
+     COPY_EMBED_LINK_FAILED: "Не удалось скопировать встроенную ссылку",
+  
+     COPY_TO_HTML_FAILED: "Не удалось скопировать встроенную ссылку",
+     COPY_TO_HTML_SUCCESS: "Скопировано в HTML",
+     COPY_TO_HTML: "Скопировать в HTML",
+     PLEASE_SELECT_DATA: "Для начала, пожалуйста, выберите данные для копирования",
+  
+     IMPORT_XLSX_FILE: "Импортировать xlsx файл",
+     EXPORT_XLSX_FILE: "Экспортировать xlsx файл",
+     
+  
+    // ExcelSettingTab.ts
+    BASE_COLOR: "Основная цветовая схема",
+    BASE_COLOR_DESC: "Выберите основную цветовую схему",
+    FILE_SETTING: "Настройка файла",
+    FOLDER: "ПАПКА",
+    // Create files in this folder by default
+    FOLDER_DESC: "Создавайте файлы в этой папке по умолчанию",
+    FILENAME_PREFIX: "Префикс имени файла",
+    FILENAME_PREFIX_DESC: "Префикс имени файла",
+    FILENAME_DATE_TIME: "Имя файла в формате DateTime",
+    FILENAME_DATE_TIME_DESC: "Напишите имя файла в формате даты и времени",
+    EMBED_LINK_SETTING: "Настройка ссылки для вставки",
+    SHEET_HEIGHT: "Высота листа",
+    SHEET_HEIGHT_DESC: "Высота листа по умолчанию для отображения таблиц",
+    SHEET_SETTING: "Настройки листа",
+    ROW_HEIGHT: "Высота строки",
+    ROW_HEIGHT_DESC: "Высота строки по умолчанию",
+    COLUMN_WIDTH: "Ширина столбца",
+    COLUMN_WIDTH_DESC: "Ширина столбца по умолчанию",
+    DEFAULT_ROWS_LEN: "Количество строк по умолчанию для отображения",
+    DEFAULT_ROWS_LEN_DESC: "Количество строк для отображения по умолчанию",
+    DEFAULT_COLS_LEN: "Количество столбцов по умолчанию для отображения",
+    DEFAULT_COLS_LEN_DESC: "Количество столбцов для отображения по умолчанию",
+    SHOW_SHEET_BUTTON: "Показать кнопку листа",
+    SHOW_SHEET_BUTTON_DESC: "Показать кнопку листа",
+  };
+  
