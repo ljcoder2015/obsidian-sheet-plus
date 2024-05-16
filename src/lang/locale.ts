ec7ef934ee59ba68ab828406d67cb64379f52fa0
雷军 <ljcoder@163.com>
2024-05-16 14:51:30 +0800

    fix: i18n

--- src/lang/locale.ts
+++ src/lang/locale.ts
@@ -1,22 +1,22 @@
-import { LocaleType } from '@univerjs/core'
+import { LocaleType, Tools } from '@univerjs/core'
 import { enUS as UniverDesignEnUS } from '@univerjs/design'
 import { enUS as UniverDocsUIEnUS } from '@univerjs/docs-ui'
 import { enUS as UniverSheetsEnUS } from '@univerjs/sheets'
 import { enUS as UniverSheetsUIEnUS } from '@univerjs/sheets-ui'
+import { enUS as UniverFindReplaceEnUS } from '@univerjs/find-replace'
+import { enUS as UniverSheetsFormulaEnUS } from '@univerjs/sheets-formula'
+import { enUS as UniverSheetsDataValidationEnUS } from '@univerjs/sheets-data-validation'
 import { enUS as UniverUiEnUS } from '@univerjs/ui'
-import { enUS as UniverFindReplace } from '@univerjs/find-replace'
-import { enUS as UniverSheetFindReplace } from '@univerjs/sheets-find-replace'
-import { enUS as UniverDataValidation } from '@univerjs/sheets-data-validation'
 
 export const locales = {
-  [LocaleType.EN_US]: {
-    ...UniverSheetsEnUS,
-    ...UniverDocsUIEnUS,
-    ...UniverSheetsUIEnUS,
-    ...UniverUiEnUS,
-    ...UniverDesignEnUS,
-    ...UniverFindReplace,
-    ...UniverSheetFindReplace,
-    ...UniverDataValidation,
-  },
+  [LocaleType.EN_US]: Tools.deepMerge(
+    UniverSheetsEnUS,
+    UniverDocsUIEnUS,
+    UniverFindReplaceEnUS,
+    UniverSheetsUIEnUS,
+    UniverSheetsFormulaEnUS,
+    UniverSheetsDataValidationEnUS,
+    UniverUiEnUS,
+    UniverDesignEnUS,
+  ),
 }
